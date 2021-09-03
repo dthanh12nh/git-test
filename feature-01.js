5cfcef33f765579a82007579f2683d06bdd64100
Mot Tran <dthanh14nh@gmail.com>
2021-09-03 22:20:56 +0700

    console 7

--- feature-01.js
+++ feature-01.js
@@ -4,3 +4,4 @@ console.log("my feature 01 again");
 console.log("my feature 01 again");
 console.log("my feature 01 again");
 console.log("my feature 01 again");
+console.log("my feature 01 again");
